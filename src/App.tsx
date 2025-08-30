4fb56928ec1b2cad65c7b00d6af6649c2caa17c6
Kamal Mahad <kmlmahad@gmail.com>
2025-08-31 00:01:09 +0200

    Oppdatert UI og fikset feil

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,6 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
           <Route path="/stats" element={<Stats />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
